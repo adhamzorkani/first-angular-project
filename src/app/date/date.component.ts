a6119f8050f3da6d7ece97bdd4ce6dd43e3eeec9
adhamzorkani <adham.zorkani@aucegypt.edu>
2023-07-12 15:51:17 +0300

    .

--- src/app/date/date.component.ts
+++ src/app/date/date.component.ts
@@ -5,6 +5,19 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './date.component.html',
   styleUrls: ['./date.component.css']
 })
-export class DateComponent{
-  
+export class DateComponent {
+  dateMessage!: String;
+  someNumber: number = 100;
+
+  constructor() {
+    setInterval(() => {
+      let currentDate = new Date();
+      this.dateMessage = currentDate.toDateString() + ' ' + currentDate.toLocaleTimeString();
+    }, 1000);
+  }
+
+  addTwoNumbers(a: number, b: number) {
+    return a + b;
+  }
+
 }
